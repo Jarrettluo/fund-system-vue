be260c9556a6f2a87e2dd856f69477f008b81b5f
Jarrett <291979254@qq.com>
2021-12-11 11:50:04 +0800

    增加趋势图，添加删除自选

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,31 +1,35 @@
 <template>
   <div class="main-body">
-    <van-form @submit="onSubmit">
-      <van-field
-          v-model="username"
-          name="用户名"
-          label="用户名"
-          placeholder="用户名"
-          :rules="[{ required: true, message: '请填写用户名' }]"
-      />
-      <van-field
-          v-model="password"
-          type="password"
-          name="密码"
-          label="密码"
-          placeholder="密码"
-          :rules="[{ required: true, message: '请填写密码' }]"
-      />
-      <div style="margin: 16px;">
-        <van-button round block type="info" native-type="submit">提交</van-button>
+    <div class="container">
+      <van-form @submit="login">
+        <van-field
+            v-model="username"
+            name="用户名"
+            label="用户名"
+            placeholder="用户名"
+            :rules="[{ required: true, message: '请填写用户名' }]"
+        />
+        <van-field
+            v-model="password"
+            type="password"
+            name="密码"
+            label="密码"
+            placeholder="密码"
+            :rules="[{ required: true, message: '请填写密码' }]"
+        />
+        <div style="margin: 16px;">
+          <van-button round block type="info" native-type="submit">提交</van-button>
+        </div>
+      </van-form>
+      <div class="info-msg" @click="toRegister">
+        <span>新用户注册</span>
       </div>
-    </van-form>
-    <div class="info-msg" @click="toRegister">
-      <span>新用户注册</span>
     </div>
   </div>
 </template>
 <script>
+  import loginPageRequest from '../request/requests'
+  import md5 from 'js-md5';
   export default {
     name: 'LoginPage',
     data() {
@@ -37,7 +41,6 @@
     methods: {
       onSubmit(values) {
         console.log('submit', values);
-        this.toMyselected()
 
       },
       toMyselected() {
@@ -45,6 +48,27 @@
       },
       toRegister() {
         this.$router.push("/register")
+      },
+      async login(){
+        let data = {
+          userName: 'luojiarui', // this.username,
+          userPassword: '123456' // this.password
+        }
+        await loginPageRequest.loginRequest(data)
+            .then(res => {
+              this.updateLoginResult(res)
+            })
+            .catch(err => {
+              console.log(err)
+            })
+      },
+      updateLoginResult(res) {
+        if(res.code==200) {
+          sessionStorage.setItem("token", res.data);
+          sessionStorage.setItem("userId", this.username);
+          sessionStorage.setItem("username", this.password);
+          this.toMyselected()
+        }
       }
     },
   };
@@ -56,21 +80,11 @@
     height: 100vh;
     //background-color: #38b5ab;38b5ab
     position: relative;
-    .van-form {
-      width: 100%;
+    .container {
       position: absolute;
-      top: calc( 50% - 200px);
+      top: calc(50% - 200px);
       left: 0px;
-    }
-    .info-msg {
-      position: absolute;
-      top: calc( 50% - 40px);
-      right: calc(50% - 35px);
-      font-size: 0.75rem;
-      color: #999999;
-      &:active {
-        color: #666;
-      }
+      width: 100%;
     }
   }
 </style>
